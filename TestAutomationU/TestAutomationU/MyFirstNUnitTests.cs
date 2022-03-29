0e78d3ffc33376ab5d7d38f82d8e9d402f7c6932
NAHID\nhdrasel <Nhdrasel@gmail.com>
2022-03-30 00:26:11 +1300

    Assertion

---
+++ TestAutomationU/TestAutomationU/MyFirstNUnitTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+
+
+namespace TestAutomationU
+{
+    public class MyFirstNUnitTests
+    {
+        [Test]
+        public void IAmDoingGreat()
+        {
+            Console.WriteLine("See you in the test output!");
+            Assert.True(true);
+        }
+        [Test]
+        public static void ThisIsAStaticTest()
+        {
+
+        }
+
+
+    }
+}
