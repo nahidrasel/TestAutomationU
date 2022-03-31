5d8a160b7731dde50a9f83b84be60b0e5da1cf08
nahidrasel <Nhdrasel@gmail.com>
2022-03-31 23:26:09 +1300

    Section 4.3

---
+++ TestAutomationU/TestAutomationU/AdvancedOptions.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace TestAutomationU
+{
+    [TestFixture]
+    public class AdvancedOptions
+    {
+        [Test]
+        public void UsingWarnings()
+        {
+            var isProcessd = false;
+            Warn.Unless(isProcessd, Is.EqualTo(true)
+                .After(1).Minutes.PollEvery(10).Seconds);
+            Console.WriteLine("still going");
+
+        }
+        [Test]
+        public void AssertPassThrowsExceptionn()
+        {
+            Assert.That(Assert.Pass, Throws.TypeOf<AssertionException>());
+
+        }
+        [Test]
+        public void Assumptions()
+        {
+            Assume.That("actual", Is.EqualTo("expected"));
+        }
+
+        [Test]
+        public void WillThisMakeItThroughCodeReview()
+
+        {
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(1, 2);
+                Assert.AreEqual(3, 4);
+
+            });
+        }
+
+
+
+
+    }
+}
