880045d490cf7075cabb51e12319e913a67ca325
nahidrasel <Nhdrasel@gmail.com>
2022-04-03 00:29:42 +1300

    Modified Parameter

--- TestAutomationU/TestWithNunit/ParameterizedTest.cs
+++ TestAutomationU/TestWithNunit/ParameterizedTest.cs
@@ -11,6 +11,18 @@ namespace TestWithNunit
             Assert.AreEqual(z,x-y);
 
         }
-        
+
+        [Test, TestCaseSource("DivideCases")]
+        public void DivideTest(int n, int d, int q)
+        {
+            Assert.AreEqual(q, n / d);
+        }
+
+        static object[] DivideCases =
+        { new object[] { 12, 3, 4 },
+            new object[] { 12, 2, 6 },
+            new object[] { 12, 4, 3 }
+        };
+
     }
 }
