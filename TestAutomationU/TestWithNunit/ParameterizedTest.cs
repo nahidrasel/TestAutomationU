0e0b0d18a58d47a8e130c94ae6cb9cdb0d5e9306
nahidrasel <Nhdrasel@gmail.com>
2022-04-03 10:35:23 +1200

    Tests with Nicolay

--- TestAutomationU/TestWithNunit/ParameterizedTest.cs
+++ TestAutomationU/TestWithNunit/ParameterizedTest.cs
@@ -6,6 +6,7 @@ namespace TestWithNunit
     public class ParameterizedTest
     {
         [TestCase(4,2,2)]
+        [TestCase(6,4,1)]
         public void AddTest(int x, int y, int z)
         {
             Assert.AreEqual(z,x-y);
